67dce7f97e107b9e45a9d86c2a5b1e268cb202cc
mohammad898 <31579129+mohammad898@users.noreply.github.com>
2018-05-09 21:35:21 -0400

    Add files via upload

---
+++ synchonized.java
@@ -0,0 +1,93 @@
+package synchonized;
+
+
+
+	public class synchonized{
+		 
+	    public static void main(String[] args) throws Throwable{
+	        economy myeconomy = new economy();
+	 
+	        Thread t1 = new Thread(new producer(myeconomy));
+	        Thread t2 = new Thread(new consumer(myeconomy));
+	 
+	        t2.start();
+	        t1.start();
+	    }
+	}
+	 
+	class producer implements Runnable{
+	    economy e;
+	 
+	    producer(economy e){
+	        this.e = e;
+	    }
+	 
+	    public void run(){
+	        try{
+	            e.producer();
+	        }catch(Throwable t){}
+	    }
+	}
+	 
+	class consumer implements Runnable{
+	    economy e;
+	 
+	    consumer(economy e){
+	        this.e = e;
+	    }
+	 
+	    public void run(){
+	        try{
+	            e.consumer();
+	        }catch(Throwable t){}
+	    }
+	}
+	 
+	class economy{
+
+	    boolean product = false;
+	 
+	    public void producer() throws Throwable{
+	        System.out.println("Running producer");
+	        
+	        synchronized (this) {
+	        	while(true){
+	        		if(product == true){
+	        			try{
+	                		System.out.println("Producer: Too much product exists. Gonna wait...");
+	                		wait();
+	        			}
+	        			catch(InterruptedException e){}
+	                
+	        		}
+	        		System.out.println("Producer: Making more product now.");
+
+	        		product = true;
+	        		notifyAll();
+	        		Thread.sleep(1000);
+	        		System.out.println("Producer ending");
+	        	}
+	        }
+	    }
+	 
+	    public void consumer() throws Throwable{
+	        System.out.println("Running Consumer");
+	        
+	        synchronized (this) {
+	        	while(true){
+	        		if(product == false){
+	        			try{
+	        				System.out.println("Consumer: No products available to consume. Gonna wait...");
+	        				wait();
+	        			}catch(InterruptedException e){}
+	        		}
+	        		
+	        		System.out.println("Consumder: Now consuming product");
+	        		product = false;
+	        		notifyAll();
+	        		Thread.sleep(10);
+	           		System.out.println("Consumer ending");
+	        	}
+	        }
+	    }
+	}
